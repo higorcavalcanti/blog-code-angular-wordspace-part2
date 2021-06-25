e733e27a57e97ea277f283017ebfc17eda506bdd
Higor Cavalcanti <higordaniell@live.com>
2021-06-25 13:16:29 -0300

    HttpConfig and HttpModule

--- projects/lib-name/src/lib/http/http.module.ts
+++ projects/lib-name/src/lib/http/http.module.ts
@@ -1,8 +1,8 @@
-import { NgModule } from '@angular/core';
+import { ModuleWithProviders, NgModule, Optional } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HttpClientModule } from '@angular/common/http';
 
-
+import { HttpConfig } from './http.config';
 
 @NgModule({
   declarations: [],
@@ -11,4 +11,19 @@ import { HttpClientModule } from '@angular/common/http';
     HttpClientModule,
   ]
 })
-export class HttpModule { }
+export class HttpModule {
+  public static forRoot(config: HttpConfig = new HttpConfig()): ModuleWithProviders<HttpModule> {
+    return {
+      ngModule: HttpModule,
+      providers: [
+        { provide: HttpConfig, useValue: config }
+      ]
+    };
+  }
+
+  constructor(@Optional() config: HttpConfig) {
+    if ( !config?.API ) {
+      throw new Error('Você precisa passar uma configuração válida (HttpConfig) para o HttpModule');
+    }
+  }
+}
